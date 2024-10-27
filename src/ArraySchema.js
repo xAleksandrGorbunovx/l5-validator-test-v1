d746774c1bb3ad70b2f26da70ab5ad0e70ae2a65
Your Name~rm <you@example.com>
2024-10-27 15:40:37 +0300

    add maxDepth motod

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -13,4 +13,18 @@ export default class ArraySchema {
     const validatorLength = (value) => (value !== null ? value.length === num : false);
     return new ArraySchema([validatorLength]);
   }
+
+  maxDepth(max) {
+    const validatorMaxDepth = (values) => {
+      const iter = (element, depth = -1) => {
+        if (!Array.isArray(element)) {
+          return depth;
+        }
+        const result = element.map((value) => iter(value, depth + 1));
+        return Math.max(...result);
+      };
+      return iter(values) <= max;
+    };
+    return new ArraySchema([validatorMaxDepth]);
+  }
 }
